fe520289b8dd764b1c6d546a75f237a20837cd9c
bynextpr <boriscom123@yandex.ru>
2021-02-12 02:53:41 +0300

    Move to GH

--- uyut/script/index.js
+++ uyut/script/index.js
@@ -1,1 +1,12 @@
 console.log('blank');
+// drop-menu
+let checkboxEl = document.getElementById('drop-menu');
+let menuButtonEl = document.getElementsByClassName('header-navigation');
+function activeDDMenu(bool){
+  if(checkboxEl.checked) {
+    checkboxEl.checked = false;
+  } else {
+    checkboxEl.checked = true;
+  }
+}
+menuButtonEl[0].addEventListener('click', function(){activeDDMenu(this);});
