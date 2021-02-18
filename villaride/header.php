67bd15a83716d0334020a6f579b9002e91d4a4a1
bynextpr <boriscom123@yandex.ru>
2021-02-18 03:07:09 +0300

    Move to GH

--- villaride/header.php
+++ villaride/header.php
@@ -16,7 +16,7 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/favicon/favicon.ico">
+  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/assets/favicon/favicon.ico">
 
 	<?php wp_head(); ?>
 </head>
